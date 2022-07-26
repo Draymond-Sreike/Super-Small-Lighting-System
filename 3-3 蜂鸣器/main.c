915d31cd06f656a635ea09d778c79669fc1438b8
nopedes <110004355+nopedes@users.noreply.github.com>
2022-07-27 03:06:12 +0800

    Create main.c

---
+++ 3-3 蜂鸣器/main.c
@@ -0,0 +1,26 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+
+int main()
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	//GPIO_SetBits(GPIOA, GPIO_Pin_0); 
+	//GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+	//GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
+	//GPIO_Write(GPIOA, 0xFFFF);
+	while(1)
+	{
+		GPIO_SetBits(GPIOB, GPIO_Pin_12); 
+//		Delay_ms(200);
+//		GPIO_ResetBits(GPIOB, GPIO_Pin_12); 
+//		Delay_ms(200);
+
+	}
+}
